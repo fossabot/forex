93f08dbb3cddfff4950ca35632c460ad2c22986a
jigiman <jigiman@gmail.com>
2021-06-20 19:53:08 +0545

    Updated test file

--- src/test/java/com/forex/euro/EuroApplicationTests.java
+++ src/test/java/com/forex/euro/EuroApplicationTests.java
@@ -1,15 +1,16 @@
 package com.forex.euro;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 class EuroApplicationTests {
 
 	@Test
 	void contextLoads() {
-
+		assertEquals(7, 7);
 	}
 
 }
