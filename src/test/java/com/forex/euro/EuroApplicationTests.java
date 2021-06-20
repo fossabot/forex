ea98104768db1e60a1f6c4d86f01bf2f8d43c831
jigiman <jigiman@gmail.com>
2021-06-20 19:42:52 +0545

    removed assets from the test file

--- src/test/java/com/forex/euro/EuroApplicationTests.java
+++ src/test/java/com/forex/euro/EuroApplicationTests.java
@@ -2,16 +2,14 @@ package com.forex.euro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
 
 @SpringBootTest
 class EuroApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals(7, 7);
+
 	}
 
 }
