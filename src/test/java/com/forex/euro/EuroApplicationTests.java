d988d17f8b2faf5f6e6d218260e2e5f4e3f41427
jigiman <jigiman@gmail.com>
2021-06-20 19:10:34 +0545

    added imports in the test file

--- src/test/java/com/forex/euro/EuroApplicationTests.java
+++ src/test/java/com/forex/euro/EuroApplicationTests.java
@@ -2,6 +2,9 @@ package com.forex.euro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 @SpringBootTest
 class EuroApplicationTests {
